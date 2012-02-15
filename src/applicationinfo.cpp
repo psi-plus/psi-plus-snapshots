798061a1b9cb088ba24251287c2e9a5ed953a612
Boris Pek <tehnick-8@yandex.ru>
2012-02-15 08:22:17 +0200

    Sources were synced with upstream. Current version is 0.15.5201

--- src/applicationinfo.cpp
+++ src/applicationinfo.cpp
@@ -34,9 +34,9 @@
 
 #define PROG_NAME "Psi+"
 #ifdef WEBKIT
-#define PROG_VERSION "0.15.5196-webkit" " (" __DATE__ ")" //CVS Builds are dated
+#define PROG_VERSION "0.15.5201-webkit" " (" __DATE__ ")" //CVS Builds are dated
 #else
-#define PROG_VERSION "0.15.5196" " (" __DATE__ ")" //CVS Builds are dated
+#define PROG_VERSION "0.15.5201" " (" __DATE__ ")" //CVS Builds are dated
 #endif
 //#define PROG_VERSION "0.15";
 #define PROG_CAPS_NODE "http://psi-dev.googlecode.com/caps"
