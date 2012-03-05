9008eba04cc4d2ddb3dae7681664f5967ed2b7cf
Boris Pek <tehnick-8@yandex.ru>
2012-03-05 19:17:20 +0200

    -e

--- src/applicationinfo.cpp
+++ src/applicationinfo.cpp
@@ -34,9 +34,9 @@
 
 #define PROG_NAME "Psi+"
 #ifdef WEBKIT
-#define PROG_VERSION "0.15.5235-webkit" " (" __DATE__ ")" //CVS Builds are dated
+#define PROG_VERSION "0.15.5237-webkit" " (" __DATE__ ")" //CVS Builds are dated
 #else
-#define PROG_VERSION "0.15.5235" " (" __DATE__ ")" //CVS Builds are dated
+#define PROG_VERSION "0.15.5237" " (" __DATE__ ")" //CVS Builds are dated
 #endif
 //#define PROG_VERSION "0.15";
 #define PROG_CAPS_NODE "http://psi-dev.googlecode.com/caps"
