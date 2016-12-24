0176a921b00df8a7cd895e8f4cab2a2be9d90b50
Boris Pek <tehnick-8@yandex.ru>
2016-12-24 16:15:09 +0300

    Current version is 0.16.568.615: Psi+ is updated. Plugins are updated.

--- src/psiaccount.cpp
+++ src/psiaccount.cpp
@@ -5162,7 +5162,11 @@ void PsiAccount::handleEvent(const PsiEvent::Ptr &e, ActivationType activationTy
 				e->setOriginLocal(true);
 				doPopup = false;
 			}
-
+			// throw away carbons sent to another full JID, this happens with MUC private messages carbons
+			// so we have to explicitly skip them or we'll have a lot of duplicates
+			if (m.carbonDirection() == Message::Received && d->jid != m.to()) {
+				return;
+			}
 			ChatDlg *c = findChatDialogEx(chatJid, m.carbonDirection() == Message::Sent);
 			if (c && c->jid().resource().isEmpty())
 				c->setJid(chatJid);
