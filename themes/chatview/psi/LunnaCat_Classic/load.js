e985c1872b6fb03e06545311c70dffbc393a0edb
Boris Pek <tehnick-8@yandex.ru>
2017-06-01 00:15:10 +0300

    Current version is 1.0.2.795:

--- themes/chatview/psi/LunnaCat_Classic/load.js
+++ themes/chatview/psi/LunnaCat_Classic/load.js
@@ -1,1 +1,7 @@
-chatServer.setMetaData({name: "LunnaCat Classic 1.1"});
+srvLoader.setMetaData({
+	name: "LunnaCat Classic",
+	version: "1.2",
+	authors: ["majik <maksim.maj@gmail.com>","Sergey Ilinykh <rion4ik@gmail.com>"],
+	description: "Classic dark theme with nice background.",
+	url: "http://psi-im.org"
+});
