eaf04065755d0b89e43f150e4465e42f8564e1fb
Boris Pek <tehnick-8@yandex.ru>
2019-05-17 22:20:12 +0300

    Current version of Psi+ is 1.4.694
    It is based on:
    * psi: 6635c0cc
    * patches: 280e8c3
    * plugins: 302a2d7
    * resources: 68eed9b

--- iris/src/xmpp/xmpp-im/jingle.cpp
+++ iris/src/xmpp/xmpp-im/jingle.cpp
@@ -1328,6 +1328,7 @@ void Session::initiate()
 
 void Session::terminate(Reason::Condition cond, const QString &comment)
 {
+    d->state = State::Finishing;
     d->terminateReason = Reason(cond, comment);
 }
 
