af160915d8816c52c292c366edd253320b3dc253
Boris Pek <tehnick-8@yandex.ru>
2020-05-30 21:50:08 +0300

    Current version of Psi+ is 1.4.1330
    It is based on:
    * psi: 0fd822e1
    * patches: 2170e90
    * plugins: 863a84d
    * psimedia: 7eeeafe
    * resources: fd9879d

---
+++ src/svgiconengine.cpp
@@ -0,0 +1,80 @@
+/*
+ * svgiconengine.cpp - icon engine designed to be used with PsiIcon
+ * Copyright (C) 2020  Sergey Ilinykh
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "svgiconengine.h"
+
+#include <QPainter>
+
+QSize SvgIconEngine::actualSize(const QSize &size, QIcon::Mode mode, QIcon::State state)
+{
+    Q_UNUSED(mode);
+    Q_UNUSED(state);
+    return renderer->defaultSize().scaled(size, Qt::KeepAspectRatio);
+}
+
+QIconEngine *SvgIconEngine::clone() const { return new SvgIconEngine(name, renderer); }
+
+void SvgIconEngine::paint(QPainter *painter, const QRect &rect, QIcon::Mode mode, QIcon::State state)
+{
+    Q_UNUSED(mode);
+    Q_UNUSED(state);
+    auto r = rect.isEmpty() ? QRect(0, 0, painter->device()->width(), painter->device()->height()) : rect;
+    renderer->render(painter, r);
+}
+
+void SvgIconEngine::virtual_hook(int id, void *data)
+{
+    switch (id) {
+    case QIconEngine::AvailableSizesHook:
+        reinterpret_cast<AvailableSizesArgument *>(data)->sizes.clear();
+        break;
+    case QIconEngine::IconNameHook:
+        *reinterpret_cast<QString *>(data) = name;
+        break;
+#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+    case QIconEngine::IsNullHook:
+        *reinterpret_cast<bool *>(data) = !(renderer && renderer->isValid());
+        break;
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(5, 9, 0)
+    case QIconEngine::ScaledPixmapHook: {
+        auto arg    = reinterpret_cast<ScaledPixmapArgument *>(data);
+        arg->pixmap = renderPixmap(arg->size * arg->scale, arg->mode, arg->state);
+        break;
+    }
+#endif
+    }
+}
+
+QPixmap SvgIconEngine::pixmap(const QSize &size, QIcon::Mode mode, QIcon::State state)
+{
+    return renderPixmap(size, mode, state);
+}
+
+QPixmap SvgIconEngine::renderPixmap(const QSize &size, QIcon::Mode mode, QIcon::State state)
+{
+    Q_UNUSED(mode)
+    Q_UNUSED(state)
+    auto    sz = renderer->defaultSize().scaled(size, Qt::KeepAspectRatio);
+    QPixmap pix(sz);
+    pix.fill(Qt::transparent);
+    QPainter p(&pix);
+    renderer->render(&p);
+    return pix;
+}
