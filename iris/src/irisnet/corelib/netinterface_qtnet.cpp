c9b7820b28c089fe3755ed7aaff2ea59d9210722
Boris Pek <tehnick-8@yandex.ru>
2020-11-01 00:50:13 +0300

    Current version of Psi+ is 1.4.1530
    It is based on:
    * psi: d38e5614
    * patches: 2170e90
    * plugins: 5100bca
    * psimedia: c24f0fe
    * resources: a00b363

--- iris/src/irisnet/corelib/netinterface_qtnet.cpp
+++ iris/src/irisnet/corelib/netinterface_qtnet.cpp
@@ -20,20 +20,95 @@
 
 #include <QNetworkInterface>
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
+#ifdef Q_OS_LINUX
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+class InterfaceMonitor : public QObject {
+    Q_OBJECT
+public:
+    InterfaceMonitor()
+    {
+        struct sockaddr_nl sa;
+
+        memset(&sa, 0, sizeof(sa));
+        sa.nl_family = AF_NETLINK;
+        sa.nl_groups = RTMGRP_LINK | RTMGRP_IPV4_IFADDR | RTMGRP_IPV6_IFADDR;
+
+        netlinkFd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+        if (netlinkFd == -1) {
+            return;
+        }
+        if (bind(netlinkFd, (struct sockaddr *)&sa, sizeof(sa)) == -1) {
+            close(netlinkFd);
+            netlinkFd = -1;
+            return;
+        }
+        notifier = new QSocketNotifier(netlinkFd, QSocketNotifier::Read, this);
+
+        connect(notifier, &QSocketNotifier::activated, this,
+                [=](QSocketDescriptor, QSocketNotifier::Type) { emit changed(); });
+    }
+
+    ~InterfaceMonitor()
+    {
+        if (notifier) {
+            delete notifier;
+            close(netlinkFd);
+        }
+    }
+
+signals:
+    void changed();
+
+private:
+    QSocketNotifier *notifier  = nullptr;
+    int              netlinkFd = -1;
+};
+
+#else
+// not linux version. polling? TODO. probably with Qt6 we can use QNetworkStatusMonitor
+class InterfaceMonitor : public QObject {
+    Q_OBJECT
+public:
+    InterfaceMonitor() { }
+
+signals:
+    void changed();
+};
+#endif
+#else // old Qt < 5.15
+class InterfaceMonitor : public QObject {
+    Q_OBJECT
+public:
+    InterfaceMonitor()
+    {
+        connect(&ncm, SIGNAL(configurationAdded(QNetworkConfiguration)), SIGNAL(changed()));
+        connect(&ncm, SIGNAL(configurationChanged(QNetworkConfiguration)), SIGNAL(changed()));
+        connect(&ncm, SIGNAL(configurationRemoved(QNetworkConfiguration)), SIGNAL(changed()));
+    }
+
+signals:
+    void changed();
+
+private:
+    QNetworkConfigurationManager ncm;
+};
+#endif
+
 namespace XMPP {
 class IrisQtNet : public NetInterfaceProvider {
     Q_OBJECT
     Q_INTERFACES(XMPP::NetInterfaceProvider)
 public:
-    QList<Info>                  info;
-    QNetworkConfigurationManager ncm;
+    QList<Info>      info;
+    InterfaceMonitor monitor;
 
-    IrisQtNet()
-    {
-        connect(&ncm, SIGNAL(configurationAdded(QNetworkConfiguration)), SLOT(check()));
-        connect(&ncm, SIGNAL(configurationChanged(QNetworkConfiguration)), SLOT(check()));
-        connect(&ncm, SIGNAL(configurationRemoved(QNetworkConfiguration)), SLOT(check()));
-    }
+    IrisQtNet() { connect(&monitor, &InterfaceMonitor::changed, this, &IrisQtNet::check); }
 
     void start() { poll(); }
 
